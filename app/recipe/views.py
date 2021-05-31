2a6951d02afa05a157dd1652d243ada30d6c6bfc
vinay <vyvinay461as@gmail.com>
2021-05-31 23:45:36 +0530

    added recipe filter feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,8 +38,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    @staticmethod
+    def _params_to_ints(qs):
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tags_in = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tags_in)
+        if ingredients:
+            ingredients_in = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredients_in)
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
